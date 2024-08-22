5b1c0b1a2b15c2bb455f513c25952a27e9c068cd
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2024-08-22 19:31:39 +0530

    Add files via upload

---
+++ SwitchStatement/ex.java
@@ -0,0 +1,17 @@
+package SwitchStatement;
+
+public class ex {
+    public static void main(String[] args) {
+        int number=2;
+        switch (number) {
+            case 1:System.out.println("Samosa");
+            break;
+            case 2:System.out.println("Burger");
+            break;
+            case 3:System.out.println("Sandwich");
+            break;
+            default:System.out.println("It was a dream");
+                break;
+        }
+    }
+}
