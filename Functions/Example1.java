111d761b9fb7d2d202057fb08d1c3833cdad398a
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2024-08-26 10:28:51 +0530

    Add files via upload

---
+++ Functions/Example1.java
@@ -0,0 +1,11 @@
+package Functions;
+
+public class Example1 {
+    public static void Example1(){
+        System.out.println("hello world");
+        
+    }
+    public static void main(String[] args) {
+        Example1();//Function Call
+    }
+}
