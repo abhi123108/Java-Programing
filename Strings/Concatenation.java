0f2bf662f60da949000ab2c1f5a177aa180f88e2
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2024-09-18 18:45:44 +0530

    Add files via upload

---
+++ Strings/Concatenation.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Concatenation {
+    public static void main(String[] args) {
+        String firstName="Abhinav";
+        String lastName = "Singh";
+        String fullName=firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
