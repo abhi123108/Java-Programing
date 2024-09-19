34b3f10b3558569520af7e3c1624725cb1106db6
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2024-09-19 14:09:06 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class StringLength {
+    public static void main(String[] args) {
+        String fullname ="Abhinav Singh";
+        System.out.println(fullname.length());
+    }
+}
