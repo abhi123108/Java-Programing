73e5f5c699e9dc2d87528636f71abff0da836cc1
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2024-09-19 14:12:02 +0530

    Delete StringLength.java

--- StringLength.java
+++
@@ -1,8 +0,0 @@
-package Strings;
-
-public class StringLength {
-    public static void main(String[] args) {
-        String fullname ="Abhinav Singh";
-        System.out.println(fullname.length());
-    }
-}
