0faceb1c84602fbe0b5a2670a53dbf6ca2705a9b
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2024-09-20 23:48:24 +0530

    Add files via upload

---
+++ BitManipulation/Example1.java
@@ -0,0 +1,17 @@
+package BitManipulation;
+
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.println(5&6);   //& operator
+
+        System.out.println(5|6);   // OR operator
+
+        System.out.println(5^6);     //XOR operator
+
+        System.out.println(~5);    //NOT operator
+
+        System.out.println(5<<2);  //Binary Left Shift operator
+
+        System.out.println(6>>1);  //Binary Right Shift operator
+    }
+}
