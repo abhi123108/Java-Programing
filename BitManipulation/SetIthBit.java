3660fafea7fb645967e8f4564ebd752d0cd103bc
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2024-09-24 18:24:19 +0530

    Add files via upload

---
+++ BitManipulation/SetIthBit.java
@@ -0,0 +1,11 @@
+package BitManipulation;
+
+public class SetIthBit {
+    public static int setIthbit(int n , int i){
+        int bitMask= 1<<i;
+        return n|bitMask;
+    }
+    public static void main(String[] args) {
+        System.out.println(setIthbit(10, 2));
+    }
+}
