2afd12a740bbc1eb7cf6dd91068fc89ab3707d88
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2024-09-30 23:14:32 +0530

    Add files via upload

---
+++ BitManipulation/CheckPowerOfTwo.java
@@ -0,0 +1,10 @@
+package BitManipulation;
+//Check if a number is a power of 2 or not.
+public class CheckPowerOfTwo {
+    public static boolean isPowerOfTwo(int n){
+        return (n &(n-1))==0;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(8));
+    }
+}
