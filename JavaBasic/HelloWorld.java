4c1756cd9f84059a2a506d768abd1ccfdc2779f0
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2024-10-04 22:15:54 +0530

    Update HelloWorld.java

--- JavaBasic/HelloWorld.java
+++ JavaBasic/HelloWorld.java
@@ -1,4 +1,5 @@
 public class HelloWorld {
+    //first programme
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World");
