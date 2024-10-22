fe0ff5265ed0074f787981d14f75b2203d0ed9a1
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2024-10-22 22:20:20 +0530

    Add files via upload

---
+++ BackTracking/Problem3.java
@@ -0,0 +1,23 @@
+package BackTracking;
+//Find & print permutation of a string.
+public class Problem3 {
+    public static void permutation(String str , String ans ){
+        if (str.length()==0) {
+            System.out.println(ans);
+            return;
+        }
+
+        //recursion
+        for(int i=0; i<str.length(); i++){
+            char curr=str.charAt(i);
+            String Newstr=str.substring(0, i)+str.substring(i+1);
+            permutation(Newstr, ans+curr);
+        }
+    }
+    public static void main(String[] args) {
+        String str="abc";
+        permutation(str, "");
+    }
+}
+
+//TC=O(n*n!)
