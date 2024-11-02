d2d104c13860cd8897df59fd413a9250932a5902
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2024-11-02 22:03:17 +0530

    Add files via upload

---
+++ ArrList/Classroom.java
@@ -0,0 +1,32 @@
+package ArrList;
+import java.util.ArrayList;
+public class Classroom {
+    public static void main(String[] args) {
+        ArrayList<Integer> list= new ArrayList<>();
+    ArrayList<String> list1= new ArrayList<>();
+
+    list.add(1);  //O(1)
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    
+
+    //list.add(1,5);  //O(n)
+    // getElement   O(1)
+    int element=list.get(1);
+    System.out.println(element);
+
+    //Remove element  O(n)
+    list.remove(2);
+    System.out.println(list);
+
+    //Set element at index   O(n)
+    list.set(2 ,10);
+    System.out.println(list);
+
+    //contain    O(n)
+    System.out.println(list.contains(1));
+    System.out.println(list.contains(11));
+    }
+}
