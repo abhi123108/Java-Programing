3f97c9754ef15fbed9e34dcf1bed51ac44bee8a3
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2024-11-16 22:33:41 +0530

    Add files via upload

---
+++ OOPs/MethodOverloading.java
@@ -0,0 +1,24 @@
+package OOPs;
+
+public class MethodOverloading {     //compile time polymorphism
+    public static void main(String[] args) {
+        Calculator calc= new Calculator();
+        System.out.println(calc.sum(1, 2));
+        System.out.println(calc.sum((float)1.5,(float)3.6));
+        System.out.println(calc.sum(1,2,3));
+    }
+}
+
+class  Calculator {
+    int sum(int a , int b){
+        return a+b;
+    }
+    
+    float sum(float a, float b){
+        return a+b;
+    }
+    
+    int sum(int a , int b, int c){
+        return a+b+c;
+    }
+}
