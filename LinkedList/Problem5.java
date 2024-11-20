7dc74ab37ea8fcdfd8e9d18fd46e9c364b8841a4
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2024-11-20 22:56:06 +0530

    Add files via upload

---
+++ LinkedList/Problem5.java
@@ -0,0 +1,97 @@
+package LinkedList;
+import java.util.*;
+
+// Zig Zag LinkedList
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Problem5 {
+    public static Node head;
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void zigZag() {
+        // Find mid
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        // Reverse 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+        // Alternate merge - zig zag merge
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem5 ll = new Problem5();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addLast(6);
+
+        System.out.println("Original LinkedList:");
+        ll.print();
+
+        ll.zigZag();
+
+        System.out.println("Zig Zag LinkedList:");
+        ll.print();
+    }
+}
+
